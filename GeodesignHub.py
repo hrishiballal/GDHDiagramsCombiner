b64ad815d151fbba5a5cc3589f6219cf39fd1ada
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2016-03-05 21:13:32 +0000

    updated gdh client

--- GeodesignHub.py
+++ GeodesignHub.py
@@ -21,25 +21,25 @@ class GeodesignHubClient():
 		r = requests.get(securl, headers=headers)
 		return r
 
-	def post_as_diagram(self,geoms, projectorpolicy, featuretype, description, reqid ):
+	def post_as_diagram(self,geometry, projectorpolicy, featuretype, description, reqid ):
 		''' Create a requests object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'requirements'+'/'+ str(reqid) + '/'+ 'add' +'/' + projectorpolicy +'/'
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
-		postdata = {'geometry':geoms, 'description':description, 'featuretype':featuretype}
+		postdata = {'geometry':geometry, 'description':description, 'featuretype':featuretype}
 		r = requests.post(securl, headers= headers, data = json.dumps(postdata))
 		return r
 
-	def post_as_ealuation_JSON(self, geoms, reqid, username=None):
+	def post_as_ealuation_JSON(self, geometry, reqid, username=None):
 		''' Create a requests object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'requirements'+'/'+ str(reqid) + '/e/map/json/'
 		if username:
 			securl += username +'/'
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
 
-		r = requests.post(securl, headers= headers, data = json.dumps(geoms))
+		r = requests.post(securl, headers= headers, data = json.dumps(geometry))
 		return r
 
-	def post_as_impact_JSON(self, geoms, reqid, username=None):
+	def post_as_impact_JSON(self, geometry, reqid, username=None):
 		''' Create a requests object with correct headers and creds. '''
 
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'requirements'+'/'+ str(reqid) + '/i/map/json/'
@@ -47,25 +47,25 @@ class GeodesignHubClient():
 			securl += username +'/'
 
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
-		r = requests.post(securl, headers= headers, data = json.dumps(geoms))
+		r = requests.post(securl, headers= headers, data = json.dumps(geometry))
 		return r
 
-	def post_as_evaluation_GBF(self, geoms, reqid, username=None):
+	def post_as_evaluation_GBF(self, geometry, reqid, username=None):
 		''' Create a requests object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'requirements'+'/'+ str(reqid) + '/e/map/gbf/'
 		if username:
 			securl += username +'/'
 		headers = {'Authorization': 'Token '+ self.token}
 
-		r = requests.post(securl, headers= headers, files = {'geoms.gbf':geoms})
+		r = requests.post(securl, headers= headers, files = {'geoms.gbf':geometry})
 		return r
 
-	def post_as_impact_GBF(self, geoms, reqid, username=None):
+	def post_as_impact_GBF(self, geometry, reqid, username=None):
 		''' Create a requests object with correct headers and creds. '''
 
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'requirements'+'/'+ str(reqid) + '/i/map/gbf/'
 		if username:
 			securl += username +'/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.post(securl, headers= headers, files = {'geoms.gbf':geoms})
+		r = requests.post(securl, headers= headers, files = {'geoms.gbf':geometry})
 		return r
