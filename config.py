50ca00e6cab349d8c2ff3911d2455570f4bf3451
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2016-03-15 17:47:27 +0000

    updated config file with correct url

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 apisettings = {
-  "serviceurl": "http://local.dev:8000",
+  "serviceurl": "http://local.dev:8000/api/v1/",
   "projectid": "91cb24d7cd1feb2b",
   "apitoken": "6858b59a256e5dc0028dd3261dafb2e1c1ead912"
 }
